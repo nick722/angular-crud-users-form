519f1fb0b807f30cabc8243bef12063393a30791
Nikolai Yakuschenko <nikolai.yakuschenko@gmail.com>
2018-04-26 23:34:32 +0300

    Create local form with users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './shared/models/user';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  users: User [] = [
+    {id: 1, name: 'Piper', lastname: 'One'},
+    {id: 2, name: 'Nick', lastname: 'Two'},
+    {id: 3, name: 'Preston', lastname: 'Three'}
+  ];
+  activeUser: User;
+
+  selectUser(user) {
+    this.activeUser = user;
+    console.log(this.activeUser);
+  }
+
+  onUserCreated(event) {
+    this.users.push(event.user);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import  { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserProfileComponent,
+    UserFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
