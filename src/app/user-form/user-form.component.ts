cb652ce712b14aaa2b5098caeb1ebd81bf335430
Nikolai Yakuschenko <nikolai.yakuschenko@gmail.com>
2018-04-26 23:35:55 +0300

    Create local form with users

---
+++ src/app/user-form/user-form.component.ts
@@ -0,0 +1,22 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+import { User } from '../shared/models/user';
+
+@Component({
+  selector: 'app-user-form',
+  templateUrl: './user-form.component.html',
+  styleUrls: ['./user-form.component.css']
+})
+export class UserFormComponent {
+  @Output() userCreated = new EventEmitter();
+  newUser: User = new User();
+  active: boolean = true;
+
+  onSubmit() {
+    // show the event that the user was created
+    this.userCreated.emit({user: this.newUser});
+
+    this.newUser = new User();
+    this.active = false;
+    setTimeout(() => this.active = true, 0);
+  }
+}
